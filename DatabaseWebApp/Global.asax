e1d9d5ef84756e9cd04b7d9a94f346ce5fc071a0
tekier <tawqir.sohail@gmail.com>
2016-10-17 10:52:36 +0100

    added empty MVC project to launch web app

---
+++ DatabaseWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DatabaseWebApp.MvcApplication" Language="C#" %>
---
+++ DatabaseWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DatabaseWebApp.Startup))]
+namespace DatabaseWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
