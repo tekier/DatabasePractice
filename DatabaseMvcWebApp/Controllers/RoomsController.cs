0d67ca74512702ddf1924fd573044b3ecb560fa3
tekier <tawqir.sohail@gmail.com>
2016-10-17 16:31:07 +0100

    Added viewing and filtering functionality for rooms as well

---
+++ DatabaseMvcWebApp/Controllers/RoomsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseMvcWebApp.ViewModels;
+using Microsoft.Ajax.Utilities;
+
+namespace DatabaseMvcWebApp.Controllers
+{
+    public class RoomsController : Controller
+    {
+        // GET: Rooms
+        RoomViewModel model = new RoomViewModel("Rooms", String.Empty);
+        public ActionResult Index()
+        {
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string id = null, string size = null, string floor = null, string capacity = null)
+        {
+            if (!id.IsNullOrWhiteSpace())
+            {
+                model.SearchById(id);
+            }
+            if (!size.IsNullOrWhiteSpace())
+            {
+                model.SearchByFirstName(size);
+            }
+            if (!floor.IsNullOrWhiteSpace())
+            {
+                model.SearchBySurname(floor);
+            }
+            if (!capacity.IsNullOrWhiteSpace())
+            {
+                model.SearchByDob(capacity);
+            }
+            model.RefreshTable();
+            return View(model);
+        }
+    }
+}
---
+++ DatabaseMvcWebApp/ViewModels/RoomViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DatabaseConnection;
+
+namespace DatabaseMvcWebApp.ViewModels
+{
+    public class RoomViewModel
+    {
+        public List<string> rooms;
+        public List<string> coloumnNames;
+        private DatabaseFacade facade;
+
+        public RoomViewModel(string tableName, string column)
+        {
+            facade = new DatabaseFacade();
+            rooms = facade.GetRowsUsing(tableName, column);
+            coloumnNames = new List<string>
+            {
+                "ID",
+                "Size",
+                "Floor",
+                "Capacity"
+            };
+        }
+
+        public void SearchById(string input)
+        {
+            facade.AddParameter("@RoomId", input);
+        }
+
+        public void SearchByFirstName(string input)
+        {
+            facade.AddParameter("@RoomSize", input);
+        }
+
+        public void SearchBySurname(string input)
+        {
+            facade.AddParameter("@Floor", input);
+        }
+
+        public void SearchByDob(string input)
+        {
+            facade.AddParameter("@Capacity", input);
+        }
+        public void RefreshTable()
+        {
+            rooms = facade.GetRowsUsing("Rooms", string.Empty);
+        }
+    }
+}
